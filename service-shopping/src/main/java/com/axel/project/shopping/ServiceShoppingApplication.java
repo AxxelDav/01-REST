6c5fce9919ebfb88a0ffe7e068a095a8b8b9181f
AxxelDav <102934871+AxxelDav@users.noreply.github.com>
2023-02-10 17:52:20 -0300

    Update ServiceShoppingApplication.java

--- service-shopping/src/main/java/com/axel/project/shopping/ServiceShoppingApplication.java
+++ service-shopping/src/main/java/com/axel/project/shopping/ServiceShoppingApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableFeignClients
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceShoppingApplication {
 
